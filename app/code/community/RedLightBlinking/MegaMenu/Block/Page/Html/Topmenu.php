b7cda1f7509defd3151aaf1f6778b6df530fa561
Manuele Menozzi <manuele.menozzi@gmail.com>
2015-11-16 18:05:15 +0100

    Fix for SUPEE-6788 patch and various CS fixes

--- app/code/community/RedLightBlinking/MegaMenu/Block/Page/Html/Topmenu.php
+++ app/code/community/RedLightBlinking/MegaMenu/Block/Page/Html/Topmenu.php
@@ -1,116 +1,112 @@
 <?php
 /**
- *
  * @author Enrique Piatti
- */ 
+ */
 class RedLightBlinking_MegaMenu_Block_Page_Html_Topmenu extends Mage_Page_Block_Html_Topmenu
 {
+    /**
+     * Change template if Mega Menu is enabled
+     *
+     * @return string
+     */
+    protected function _toHtml()
+    {
+        if (Mage::helper('megamenu')->isMegaMenuEnabled()) {
+            $this->setTemplate('megamenu/page/html/megamenu.phtml');
+        }
+        return parent::_toHtml();
+    }
 
+    /**
+     * include submenu items inside a div container with the possibility to add a static block there
+     *
+     * @param Varien_Data_Tree_Node $menuTree
+     * @param string $childrenWrapClass
+     * @return string
+     */
+    protected function _getHtml(Varien_Data_Tree_Node $menuTree, $childrenWrapClass)
+    {
+        if (!Mage::helper('megamenu')->isMegaMenuEnabled()) {
+            return parent::_getHtml($menuTree, $childrenWrapClass);
+        }
 
-	/**
-	 * Change template if Mega Menu is enabled
-	 *
-	 * @return string
-	 */
-	protected function _toHtml()
-	{
-		if(Mage::helper('megamenu')->isMegaMenuEnabled()){
-			$this->setTemplate('megamenu/page/html/megamenu.phtml');
-		}
-		return parent::_toHtml();
-	}
+        $html = '';
 
+        $children = $menuTree->getChildren();
+        $parentLevel = $menuTree->getLevel();
+        $childLevel = is_null($parentLevel) ? 0 : $parentLevel + 1;
 
-	/**
-	 * include submenu items inside a div container with the possibility to add a static block there
-	 *
-	 * @param Varien_Data_Tree_Node $menuTree
-	 * @param string $childrenWrapClass
-	 * @return string
-	 */
-	protected function _getHtml(Varien_Data_Tree_Node $menuTree, $childrenWrapClass)
-	{
-		if( ! Mage::helper('megamenu')->isMegaMenuEnabled()){
-			return parent::_getHtml($menuTree, $childrenWrapClass);
-		}
+        $counter = 1;
+        $childrenCount = $children->count();
 
-		$html = '';
+        $parentPositionClass = $menuTree->getPositionClass();
+        $itemPositionClassPrefix = $parentPositionClass ? $parentPositionClass . '-' : 'nav-';
 
-		$children = $menuTree->getChildren();
-		$parentLevel = $menuTree->getLevel();
-		$childLevel = is_null($parentLevel) ? 0 : $parentLevel + 1;
+        foreach ($children as $child) {
 
-		$counter = 1;
-		$childrenCount = $children->count();
+            $child->setLevel($childLevel);
+            $child->setIsFirst($counter == 1);
+            $child->setIsLast($counter == $childrenCount);
+            $child->setPositionClass($itemPositionClassPrefix . $counter);
 
-		$parentPositionClass = $menuTree->getPositionClass();
-		$itemPositionClassPrefix = $parentPositionClass ? $parentPositionClass . '-' : 'nav-';
+            $outermostClassCode = '';
+            $outermostClass = $menuTree->getOutermostClass();
 
-		foreach ($children as $child) {
+            if ($childLevel == 0 && $outermostClass) {
+                $outermostClassCode = ' class="' . $outermostClass . '" ';
+                $child->setClass($outermostClass);
+            }
 
-			$child->setLevel($childLevel);
-			$child->setIsFirst($counter == 1);
-			$child->setIsLast($counter == $childrenCount);
-			$child->setPositionClass($itemPositionClassPrefix . $counter);
+            $html .= '<li ' . $this->_getRenderedMenuItemAttributes($child) . '>';
+            $html .= '<a href="' . $child->getUrl() . '" ' . $outermostClassCode . '><span>'
+                . $this->escapeHtml($child->getName()) . '</span></a>';
 
-			$outermostClassCode = '';
-			$outermostClass = $menuTree->getOutermostClass();
+            if ($child->hasChildren()) {
+                if (!empty($childrenWrapClass)) {
+                    $html .= '<div class="' . $childrenWrapClass . '">';
+                }
 
-			if ($childLevel == 0 && $outermostClass) {
-				$outermostClassCode = ' class="' . $outermostClass . '" ';
-				$child->setClass($outermostClass);
-			}
+                if ($childLevel == 0) {
+                    $html .= '<div class="submenu-container"><div class="container">';
+                }
 
-			$html .= '<li ' . $this->_getRenderedMenuItemAttributes($child) . '>';
-			$html .= '<a href="' . $child->getUrl() . '" ' . $outermostClassCode . '><span>'
-				. $this->escapeHtml($child->getName()) . '</span></a>';
+                $grandChildren = $child->getChildren();
+                $grandChildrenCount = $grandChildren->count();
+                $columns = ceil($grandChildrenCount / 8);
+                $columnsClass = 'columns' . $columns;
 
-			if ($child->hasChildren()) {
-				if (!empty($childrenWrapClass)) {
-					$html .= '<div class="' . $childrenWrapClass . '">';
-				}
+                $html .= '<ul class="level' . $childLevel . ' ' . $columnsClass . '">';
+                $html .= $this->_getHtml($child, $childrenWrapClass);
+                $html .= '</ul>';
 
-				if($childLevel == 0){
-					$html .= '<div class="submenu-container"><div class="container">';
-				}
+                if ($childLevel == 0) {
+                    $staticBlock = $this->_getStaticBlockHtml($child);
+                    $html .= '<div class="static-container">' . $staticBlock . '</div>';
+                    $html .= '</div></div>';
+                }
 
-				$grandChildren = $child->getChildren();
-				$grandChildrenCount = $grandChildren->count();
-				$columns = ceil($grandChildrenCount / 8);
-				$columnsClass = 'columns'.$columns;
+                $html .= '<div class="see-all"><a href="' . $child->getUrl() . '">All ' .
+                    $this->escapeHtml($child->getName()) .
+                    '</a></div>';
 
-				$html .= '<ul class="level' . $childLevel . ' '. $columnsClass .'">';
-				$html .= $this->_getHtml($child, $childrenWrapClass);
-				$html .= '</ul>';
+                if (!empty($childrenWrapClass)) {
+                    $html .= '</div>';
+                }
+            }
 
-				if($childLevel == 0){
-					$staticBlock = $this->_getStaticBlockHtml($child);
-					$html .= '<div class="static-container">'.$staticBlock.'</div>';
-					$html .= '</div></div>';
-				}
+            $html .= '</li>';
 
-				$html .= '<div class="see-all"><a href="'. $child->getUrl() .'">All '. $this->escapeHtml($child->getName()) .'</a></div>';
+            $counter++;
+        }
 
-				if (!empty($childrenWrapClass)) {
-					$html .= '</div>';
-				}
-			}
+        return $html;
+    }
 
-			$html .= '</li>';
-
-			$counter++;
-		}
-
-		return $html;
-	}
-
-
-	protected function _getStaticBlockHtml($child)
-	{
-		$nodeId = $child->getId();
-		$prefix = 'category-node-';
-		$categoryId = substr($nodeId, strlen($prefix));
-		return Mage::helper('megamenu')->getMegaMenuStaticBlockHtml($categoryId);
-	}
-
+    protected function _getStaticBlockHtml($child)
+    {
+        $nodeId = $child->getId();
+        $prefix = 'category-node-';
+        $categoryId = substr($nodeId, strlen($prefix));
+        return Mage::helper('megamenu')->getMegaMenuStaticBlockHtml($categoryId);
+    }
 }
--- app/code/community/RedLightBlinking/MegaMenu/Helper/Data.php
+++ app/code/community/RedLightBlinking/MegaMenu/Helper/Data.php
@@ -2,40 +2,39 @@
 /**
  *
  * @author Enrique Piatti
- */ 
+ */
+
 class RedLightBlinking_MegaMenu_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    const MEGA_MENU_CONFIG = 'catalog/navigation/megamenu';
 
-	const MEGA_MENU_CONFIG = 'catalog/navigation/megamenu';
-
-	public function isMegaMenuEnabled($store = null)
-	{
-		return Mage::getStoreConfig(self::MEGA_MENU_CONFIG, $store);
-	}
+    public function isMegaMenuEnabled($store = null)
+    {
+        return Mage::getStoreConfig(self::MEGA_MENU_CONFIG, $store);
+    }
 
 
-	public function getMegaMenuStaticBlockName($categoryId)
-	{
-		return 'megamenu_category_'.$categoryId;
-	}
+    public function getMegaMenuStaticBlockName($categoryId)
+    {
+        return 'megamenu_category_' . $categoryId;
+    }
 
-	public function getMegaMenuStaticBlock($categoryId, $store = null)
-	{
-		$staticBlockIdentifier = $this->getMegaMenuStaticBlockName($categoryId);
-		$store = $store ? : Mage::app()->getStore()->getId();
-		$block = Mage::getModel('cms/block')
-			->setStoreId($store)
-			->load($staticBlockIdentifier);
-		return $block;
-	}
+    public function getMegaMenuStaticBlock($categoryId, $store = null)
+    {
+        $staticBlockIdentifier = $this->getMegaMenuStaticBlockName($categoryId);
+        $store = $store ?: Mage::app()->getStore()->getId();
+        $block = Mage::getModel('cms/block')
+            ->setStoreId($store)
+            ->load($staticBlockIdentifier);
+        return $block;
+    }
 
-	public function getMegaMenuStaticBlockHtml($categoryId)
-	{
-		$staticBlockIdentifier = $this->getMegaMenuStaticBlockName($categoryId);
-		$html = Mage::app()->getLayout()->createBlock('cms/block')
-			->setBlockId($staticBlockIdentifier)
-			->toHtml();
-		return $html;
-	}
-
+    public function getMegaMenuStaticBlockHtml($categoryId)
+    {
+        $staticBlockIdentifier = $this->getMegaMenuStaticBlockName($categoryId);
+        $html = Mage::app()->getLayout()->createBlock('cms/block')
+            ->setBlockId($staticBlockIdentifier)
+            ->toHtml();
+        return $html;
+    }
 }
